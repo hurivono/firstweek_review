d3d2694f63969542e0b322920567f9e0ae56ce14
hurivono <jhyoon9966@gmail.com>
2024-10-13 14:28:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-  
+@app.route('/')
+def index():
+    pod_name = os.environ.get('POD_NAME', 'Unknown Pod')
+    node_name = os.environ.get('NODE_NAME', 'Unknown Pod')
+    pod_namespace = os.environ.get('POD_NAMESPACE', 'Unknown Namespace')
+    
+    return f'Pod Name: {pod_name}<br>Node Name: {node_name}<br>Pod Namespace: {pod_namespace}, version: 241013_01'
+
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
